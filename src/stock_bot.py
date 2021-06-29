fabb52b6bbe7c081f9d6c5f686034358a43f49db
orelts <oreltsioni@gmail.com>
2021-06-29 12:12:35 +0300

    stock bot documantation

--- src/stock_bot.py
+++ src/stock_bot.py
@@ -13,14 +13,32 @@ warnings.filterwarnings('ignore')
 sys.path.append('../')
 
 
+
 def buy_and_sell(orig, pred, thresh_buy,thresh_sell=None, money=10000):
+    """Stock bot routine that buy and sell stock holds through time
+
+    Parameters:
+    orig : original stock data array
+    pred : predicted stock data array
+    thresh_buy: percent threshold for buying stock
+    thresh_sell: percent threshold for selling stock
+    money: money the bot starts with for trading
+
+    Returns:
+    left: Buy and Hold amount for stock during the given period
+    money_accum: total money the bot holds (with stock) for time
+    pct_change: last pct change of the stock
+    buy_sell_points: buying and selling points. -1 are selling points 1 are buying points and 0 are passive
+    decision_outcome: percent of profit or loss against passive approach for the given descision
+   """
     actual = orig
     predicted = pred
     number_of_stocks = (int)(money / actual[0])
-    left = money - (int)(money / actual[0]) * actual[0] + actual[len(actual) - 1] * number_of_stocks
+    left = money - (int)(money / actual[0]) * actual[0] + actual[len(actual) - 1] * number_of_stocks # buy and hold amount can be calculated now
 
-    number_of_stocks = 0
+    number_of_stocks = 0 # we start with 0 stocks
 
+    # its optional that threshold of buy and sell are automatically the same
     buying_percentage_threshold = thresh_buy
     if thresh_sell is None:
         selling_percentage_threshold = thresh_buy
@@ -30,31 +48,30 @@ def buy_and_sell(orig, pred, thresh_buy,thresh_sell=None, money=10000):
     money_accum = [money]
     decision_outcome = np.zeros(len(actual))
     buy_sell_points = np.zeros(len(actual))
-    for i in range(len(actual) - 1):
-        pct_change = 100 * ((predicted[i + 1]/actual[i]) - 1)
-        buy_sell_points[i] = 0
-        # print("actual {} pred {} pct {}".format(actual[i], predicted[i + 1], pct_change))
-        num_of_stocks_prop = int(50 * abs(pct_change))
-        # print("current balance is {} with {} stocks and stocks prop {} pct_change {} in iter {}".format(money,number_of_stocks, num_of_stocks_prop, pct_change, i))
+    for i in range(len(actual) - 1): # we loop through all data set (all minutes)
+        pct_change = 100 * ((predicted[i + 1]/actual[i]) - 1) #we predict the change of the stock value against current value
+        buy_sell_points[i] = 0 # for start this point is neither buy or selling point
+        num_of_stocks_prop = int(50 * abs(pct_change)) # this value is the initial number of stocks we intend to buy or sell and proportional to pct_change
         next_hold = money + number_of_stocks * actual[i + 1] # what amount should be in the next step in case we don't do anything
+
         if pct_change > buying_percentage_threshold:
-            for j in range(num_of_stocks_prop, 0, -1):
-                #Buying of stock
+            # we passed buying thresh
+            for j in range(num_of_stocks_prop, 0, -1): # trying to buy from num_of_stocks_prop to 0
                 if (money >= j * actual[i]):
                     money -= j * actual[i]
                     number_of_stocks += j
-                    buy_sell_points[i] = 1
+                    buy_sell_points[i] = 1 # buying point marked as 1 in this list
                     break
         elif pct_change < -selling_percentage_threshold:
+            # we passed selling thresh
             for j in range(num_of_stocks_prop, 0, -1):
-                #Selling of stock
                 if (number_of_stocks >= j):
                     money += j * actual[i]
                     number_of_stocks -= j
-                    buy_sell_points[i] = -1
+                    buy_sell_points[i] = -1 #selling point marked as -1 in this list
                     break
-        next_act = money + number_of_stocks * actual[i + 1]
-        money_accum.append(money + number_of_stocks * actual[i])
+        next_act = money + number_of_stocks * actual[i + 1] # used to calculate the actual outcome of the bot action against passive approach
+        money_accum.append(money + number_of_stocks * actual[i]) # appending money amount for this time index
         decision_outcome[i + 1] = 100 *(next_act/next_hold) - 100 # perecent of amount of money lost or earned from action in compare to passive case
     money += number_of_stocks * actual[len(actual) - 1]
     money_accum.append(money)
@@ -63,16 +80,29 @@ def buy_and_sell(orig, pred, thresh_buy,thresh_sell=None, money=10000):
 
 
 def buying_and_selling_plt(orig, pred, threshold_buy, threshold_sell, money_start):
+    """ Plots Prediction over original stock data over time with bot buying and selling points
+
+    Parameters:
+    orig : original stock data array
+    pred : predicted stock data array
+    thresh_buy: percent threshold for buying stock
+    thresh_sell: percent threshold for selling stock
+    money: money the bot starts with for trading
+
+    Returns:
+   """
+
     test_orig = orig['Price']
     test_pred = pred['Price']
+
+    # using the bot
     buy_and_hold, money, y_pct_change, buy_and_sell_dates, decision_outcome = buy_and_sell(test_orig.to_numpy().flatten(), test_pred.to_numpy().flatten(), threshold_buy, threshold_sell, money_start)
     # plot data prep
     buy_value = np.zeros(len(test_orig.to_numpy()))
     for idx, val in enumerate(test_orig.to_numpy()):
         if buy_and_sell_dates[idx] == 1:
             buy_value[idx] = val
-    buy_idx = test_orig.index[buy_value > 0]
-
+    buy_idx = test_orig.index[buy_value > 0] # buying scatter for where this vec( buy and sell dates) is 1
     buy_value = buy_value[buy_value > 0]
 
     sell_value = np.zeros(len(test_orig.to_numpy()))
@@ -80,16 +110,16 @@ def buying_and_selling_plt(orig, pred, threshold_buy, threshold_sell, money_star
         if buy_and_sell_dates[idx] == -1:
             sell_value[idx] = val
 
-    sell_idx = test_orig.index[sell_value > 0]
+    sell_idx = test_orig.index[sell_value > 0] # selling scatter for where this vec( buy and sell dates) is -1
     sell_value = sell_value[sell_value > 0]
 
     # buying and selling points
-    # plt.plot(orig['Date'].index, test_orig.values, label="Original test data")  # plots the x and y
-    # plt.plot(orig['Date'].index, test_pred.values, label="Predicted test data")
+    plt.plot(orig['Date'].index, test_orig.values, label="Original test data")  # plots the x and y
+    plt.plot(orig['Date'].index, test_pred.values, label="Predicted test data")
     ax = orig.plot(x='Date', y='Price', label="Original test data")
     pred.plot(x='Date', y='Price', label="Predicted test data", ax= ax)
-    # plt.scatter(buy_idx, buy_value * 0.95, color='green', marker='o', label="Buying Points")
-    # plt.scatter(sell_idx, sell_value * 1.05, color='red', marker='o', label="Selling Points")
+    plt.scatter(buy_idx, buy_value * 0.95, color='green', marker='o', label="Buying Points")
+    plt.scatter(sell_idx, sell_value * 1.05, color='red', marker='o', label="Selling Points")
 
     ax.text(0.3, 0.95, "Net Worth after trading:" + str("%.2f" % money[len(money) - 1]) + "$", size=20, transform=ax.transAxes, bbox=dict(facecolor='blue', alpha=0.5))
     ax.text(0.3, 0.90,"Buy and Hold amount:" + str("%.2f" % buy_and_hold) + "$", transform=ax.transAxes, size= 20, bbox=dict(facecolor='red', alpha=0.5))
@@ -105,6 +135,18 @@ def buying_and_selling_plt(orig, pred, threshold_buy, threshold_sell, money_star
 
 ## includes animation
 def money_over_time_plt(orig, pred, threshold_buy, threshold_sell, money_start):
+    """ Plots animation of the bot money through time with stock value in another subplot
+
+    Parameters:
+    orig : original stock data array
+    pred : predicted stock data array
+    thresh_buy: percent threshold for buying stock
+    thresh_sell: percent threshold for selling stock
+    money: money the bot starts with for trading
+
+    Returns:
+   """
+
     test_orig = orig['Price']
     test_pred = pred['Price']
     buy_and_hold, money, y_pct_change, buy_and_sell_dates, decision_outcome = buy_and_sell(test_orig.to_numpy().flatten(), test_pred.to_numpy().flatten(), threshold_buy, threshold_sell, money_start)
@@ -134,6 +176,7 @@ def money_over_time_plt(orig, pred, threshold_buy, threshold_sell, money_start):
 
     ax2_text = ax2.text(0.3, -0.5, '', transform=ax1.transAxes, size=20, bbox=dict(facecolor='red', alpha=0.3))
 
+    # updating each frame of money
     def update_money(i=int):
         ax1.legend(["net worth"],fontsize=10)
         plt.sca(ax1)
@@ -144,6 +187,7 @@ def money_over_time_plt(orig, pred, threshold_buy, threshold_sell, money_start):
         p = ax1.plot(bot_df['Date'].index[:i], bot_df['Money'][:i].values)  # note it only returns the dataset, up to the point i
         p[0].set_color('r')  # set the colour of each curve
 
+    # updating each frame of stock value
     def update_stock(i=int):
         ax2.legend(["Stock Value"],fontsize=10)
         plt.sca(ax2)
@@ -159,6 +203,7 @@ def money_over_time_plt(orig, pred, threshold_buy, threshold_sell, money_start):
         update_money(i)
         update_stock(i)
 
+    # for faster animation we show every 20 frame
     show_every_x_frams = 20
     frames = np.arange(0, len(bot_df)+ 1, show_every_x_frams)
     last_frame = frames[len(frames) - 1 ]
@@ -172,7 +217,19 @@ def money_over_time_plt(orig, pred, threshold_buy, threshold_sell, money_start):
 
     plt.show()
 
+
 def outcome_of_transactions_plt(orig, pred, threshold_buy, threshold_sell, money_start):
+    """ Plots outcome graph with green points for good decisions and red for bad ones (percent of earning or losing)
+
+    Parameters:
+    orig : original stock data array
+    pred : predicted stock data array
+    thresh_buy: percent threshold for buying stock
+    thresh_sell: percent threshold for selling stock
+    money: money the bot starts with for trading
+
+    Returns:
+   """
     test_orig = orig['Price']
     test_pred = pred['Price']
     new_df = pd.DataFrame()
@@ -188,7 +245,7 @@ def outcome_of_transactions_plt(orig, pred, threshold_buy, threshold_sell, money
     # good_sum = sum(decision_outcome[decision_outcome > 0])
     # bad_sum = sum(abs(decision_outcome[decision_outcome < 0]))
 
-    good_deals_pct = 100 * count_good/(count_bad + count_good)
+    good_deals_pct = 100 * count_good/(count_bad + count_good) # calculating good deal percent
     # good_deals_pct_weight = 100 * good_sum / deals_sum
     col = np.where(decision_outcome < 0, 'r', np.where(decision_outcome > 0, 'g', "None"))
     ax = plt.gca()
@@ -208,6 +265,18 @@ def outcome_of_transactions_plt(orig, pred, threshold_buy, threshold_sell, money
 
 
 def money_for_threshold(orig, pred, money_start):
+    """ Plots Final holdings in function of the threshold (where threshold for buying and selling are equal)
+
+    Parameters:
+    orig : original stock data array
+    pred : predicted stock data array
+    thresh_buy: percent threshold for buying stock
+    thresh_sell: percent threshold for selling stock
+    money: money the bot starts with for trading
+
+    Returns:
+   """
+
     test_orig = orig['Price']
     test_pred = pred['Price']
     # money earned against threshold
